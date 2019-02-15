a654d7cb856c37d9c68e3d30d8ee752fbb92045b
kkapuzino <42265681+kkapuzino@users.noreply.github.com>
2019-02-15 10:51:27 +0200

    Add files via upload

--- sum.js
+++ sum.js
@@ -1,4 +1,12 @@
-function sum (a, b){
-    return a + b;
+function sum(a, b=0){
+    if (a === undefined || a === null){
+        throw new Error('ei parametreja');
+    }
+    const result = Number(a) + Number(b);
+    console.log(result);
+    if(Number.isNaN(result)){
+        throw new Error('ei lukuja');
+    }
+    return result;
 }
 module.exports= sum;
